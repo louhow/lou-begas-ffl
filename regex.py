6b557038909c741059e465d1b7f2550c1a91cfa8
Louis Martinez <louhow@gmail.com>
2017-08-01 18:36:21 -0500

    s-u-c-c-e-e-s

--- regex.py
+++ regex.py
@@ -1,20 +1,98 @@
 import re
 
-regex_filters = [
-	{
-		'pattern': '\d+\.	(.*) \(.* \- (.*)\)	\$(\d+)',
-		'replace': '\\1	\\2	\\3'
-	}
+class FileFilter(object):
+  def __init__(self, file, filters):
+    self.file = file
+    self.filters = filters
+
+  def get_input_file(self):
+    return self.file + "_input.txt"
+
+  def get_output_file(self):
+    return self.file + "_output.txt"
+
+
+class Filter(object):
+  def __init__(self, pattern, replace):
+    self.pattern = pattern
+    self.replace = replace
+
+file_filters = [
+  # https://www.fantasypros.com/nfl/auction-values/calculator.php
+  FileFilter(
+    'fantasypros',
+    [
+      Filter(
+        '\d+\.	(.*) \(.* \- (.*)\)	\$(\d+)',
+        '\\1  \\2  \\3'
+      )
+    ]
+  ),
+  # https://football.fantasysports.yahoo.com/f1/91636/1/prerank_auction_costs
+  FileFilter(
+    'yahoo',
+    [
+      Filter(
+        '\$\-',
+        '$0.0'
+      ),
+      Filter(
+        '.*layer Note.*\n'
+        '(.*) .* \- (RB|WR|TE|QB|K|DEF) ?.*\n'
+        '(.*\n)?'
+        '.* \d+:\d+.*\n'
+        '\$\n'
+        '\d+\n'
+        '-	\$(\d+)	\$(\d+)	\$(\d+\.\d+)	.*\n',
+        '\\1	\\2	\\5	\\6'
+      )
+    ]
+  ),
+  # http://games.espn.com/ffl/livedraftresults?sort=aav
+  FileFilter(
+    'espn',
+    [
+      Filter(
+        '\d+	(.*), ..(.?)	(RB|WR|TE|QB|K)	.*	.*	(.*)	.*	.*',
+        '\\1	\\3	\\4'
+      ),
+      Filter(
+        '.*(\+|\-).*\n', ''  # Remove defenses
+      )
+    ]
+  ),
+  # http://fantasy.nfl.com/research/rankings?leagueId=393651&statType=draftStatss
+  FileFilter(
+    'nfl',
+    [
+      Filter('View Videos\n', ''),
+      Filter('DEF', 'DEF -'),  # Ugly, but insert the hyphen to match on for all other casess
+      Filter(
+        '\d+.*\n'
+        '(.*)(RB|WR|QB|TE|DEF|K) \-.*	(\d+)',
+        '\\1	\\2	\\3'
+      )
+    ]
+  )
 ]
-with open('input.txt', 'r') as file:
-	data = file.read()
 
-print data
+constant_filters = [
+  Filter('(Jr\. )?', '')
+]
 
-for regex_filter in regex_filters:
-	print regex_filter
-	pattern = re.compile(regex_filter['pattern'])
-	data = pattern.sub(regex_filter['replace'], data)
+def apply_filters(data, filters):
+  for filt in filters:
+    pattern = re.compile(filt.pattern)
+    data = pattern.sub(filt.replace, data)
 
-with open('output.txt', 'w') as file:
-	file.write(data)
+  return data
+
+for file_filter in file_filters:
+  with open(file_filter.get_input_file(), 'r') as file:
+    data = file.read()
+
+  data = apply_filters(data, file_filter.filters)
+  data = apply_filters(data, constant_filters)
+
+  with open(file_filter.get_output_file(), 'w') as file:
+    file.write(data)
